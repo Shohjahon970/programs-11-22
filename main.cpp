667244f1a23ef7be9142d47e533753133a8399e5
Shohjahon970 <250092@newuu.uz>
2025-09-10 12:59:47 +0500

    programs 11-22

---
+++ main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+ //problem 11
+// int main() {
+//     double x=1.13e3,y=411e-4;
+//     cout<<"x="<<x<<endl;
+//     cout<<"y="<<y<<endl;
+//     return 0;
+// }
+
+// Problem 12
+// int main() {
+//  int studentsNumber=241;
+//  int group1=studentsNumber/11;
+//  int lastgroup=studentsNumber%11;
+//  cout<<"In the first group :"<<group1<<endl;
+//  cout<<"In the last group :"<<lastgroup<<endl;
+//  return 0;
+// }
+
+
+//Problem 13
+// int main() {
+//  float s;
+//  cout<<"Enter the side of the hexagon :"<<endl;
+//  cin>>s;
+//  float area=(3*1.73)/2 *s*s;
+//  cout<<"Area is "<<area<<endl;
+// }
+
+
+//problem 14
+// int main() {
+//  int num;
+//  cout<<"Enter an integer number: ";
+//  cin >> num;
+//  int num1=num+1;
+//  int num2=num1 *3;
+//  int num3=num2 -14;
+//  int num4=num3 /4;
+//  int num5=num4 -1;
+//  int num6=num5%9;
+//  num=num6;
+//  cout<<num<<endl;
+//  return 0;
+//
+// }
+
+
+//Problem 15
+// int main() {
+//  char a='A';
+//  cout<<
+// }
+// //problem 16
+// int main() {
+//  float x=17, y=1;
+//  float z=(x+(++x))/
+// }
+
+
+//Problem 17
+// int main() {
+//  float a,b;
+//  cout<<"Enter two numbers: ";
+//  cin>>a>>b;
+//  cout<<(a==b)<<endl;
+//  return 0;
+// }
+
+
+//Problem 18
+// int main() {
+//  float a;
+//  cout<<" a= ";
+//  cin>>a;
+//  float b;
+//  cout<<"b= ";
+//  cin>>b;
+//  cout<<((a<140) &&(a<b))<<endl;
+//  return 0;
+// }
+
+//Problem 19
+// int main() {
+//  float a,b,c;
+//  cout<<"Enter three numbers: ";
+//  cin>>a>>b>>c;
+//  cout<<((a>b)&&(a>c))<<endl;
+//  return 0;
+// }
+
+
+//Problem 20
+// int main() {
+//  int a,b,c,d,e;
+//  cout<<"Enter 5 integer numbers: ";
+//  cin>>a>>b>>c>>d>>e;
+//  cout<<((a>=0)||(b>=0)||(c>=0)||(d>=0)||(e>=0))<<endl;
+//  return 0;
+//
+// }
+
+//Problem 21
+// int main() {
+//  int a,b;
+//  cout<<"Enter two numbers: ";
+//  cin>>a>>b;
+//  cout<<(((a+b)>30) && ((a*b)>30));
+//  cout
+// }
